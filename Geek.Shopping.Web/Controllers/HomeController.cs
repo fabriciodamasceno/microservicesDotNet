f7afb34de55d4b3ac29822f6f4cc0563a0d3fef5
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2024-02-08 14:38:34 -0300

    Adicionando gitignore

---
+++ Geek.Shopping.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Geek.Shopping.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Geek.Shopping.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
