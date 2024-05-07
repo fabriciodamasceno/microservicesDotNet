c4da7069164a9e722a807b2b453e8a3f89c7efb7
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2024-05-07 10:10:59 -0300

    Adicionando AutoMapper ao projeto.

--- app/GeekShopping.ProductAPI/Program.cs
+++ app/GeekShopping.ProductAPI/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
 using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -10,8 +13,16 @@ var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
     connection,
-    new MySqlServerVersion(new Version(8, 0, 29)))
+    new MySqlServerVersion(new Version(8, 0)))
     );
+
+var mappingConfig = new MappingConfig();
+
+IMapper mapper = mappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
